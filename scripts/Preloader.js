a73305e799c6fbfd124c539d7476bb2fb48bde47
liamcraddock1996 <c1381084@student.southstaffs.ac.uk>
2017-05-18 11:14:45 +0100

    javascript updated
    created main menu and upload

--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -7,15 +7,28 @@ BasicGame.Preloader = function (game) {
 BasicGame.Preloader.prototype = {
 
 	preload: function () {
-        
+    //Displays a loading screen message while the assets are loaded into memory
+		this.PreloaderText = this.add.text(this.world.centerX, this.world.centerY, 'Loading...'),
+  {
+  fontsize: '96px',
+	fill: '#fff',
+	align: 'center'
+});
+this.PreloaderText.anchor.setTO(0.5,0.5);
+
+//preload the images, sprites and audio assets into memory
+this.load.image('logo', 'assets/PhaserLogo.png');
+this.load.image('starfield', 'assets/starfield.png');
+this.load.image('startButton', 'assets/startButton.png');
+this.load.image('ship', 'assets/ship.png');
 	},
 
 	create: function () {
-	
+
 	},
 
 	update: function () {
-
+      this.game.state.start('MainMenu');
 	}
 
 };
