980a8cef84bd67fdae1880263a201fd8f21234b8
liamcraddock1996 <c1381084@student.southstaffs.ac.uk>
2017-05-18 12:05:16 +0100

    corrected mistakes

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -7,10 +7,10 @@ BasicGame.Game.prototype = {
 
 	create: function () {
        //Specifying the physics of game to ARCADE
-			 this.physics.startSystem(Phaser.physics.ARCADE);
-			 //Add the starfield and logo on screen
+			 this.physics.startSystem(Phaser.Physics.ARCADE);
+			 //Adding the starfield and logo on screen
 			 this.starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
-			 //Add the ship onto the screen, set physics and boundaries
+			 //Adding the ship onto the screen, set physics and boundaries
 			 ship = this.add.sprite((this.world.width / 2), this.world.height - 50, 'ship');
 			 ship.anchor.setTO(0.5,0);
 			 this.physics.enable(ship, Phaser.Physics.ARCADE);
